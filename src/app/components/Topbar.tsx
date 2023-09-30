7b576e3e26d7ea51319336c46cb6f8243b149890
bunrealcua <voduynhan.123@gmail.com>
2023-09-30 09:10:23 +0700

    create top bar homepage

---
+++ src/app/components/Topbar.tsx
@@ -0,0 +1,42 @@
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import styles from '../layouts/Topbar.module.css';
+import Image from 'next/image';
+import SearchIcon from '@mui/icons-material/Search';
+
+export function Topbar() {
+	return (
+		<div className={styles.container}>
+			<h1>Social</h1>
+			<div className={styles.leftContainer}>
+				<div className={styles.searchHomepageTimelineContainer}>
+					<div className={styles.searchBar}>
+						<SearchIcon style={{ color: '#000000' }} />
+						<input
+							name="search-bar"
+							placeholder="Search for friend, post or video"
+							className={styles.input}
+						/>
+					</div>
+
+					<div className={styles.homepageTimelineContainer}>
+						<a href="/">Homepage</a>
+						<a href="/">Timeline</a>
+					</div>
+				</div>
+				<div className={styles.friendMessageNotificationAvatarContainer}>
+					<PersonIcon />
+					<ChatIcon />
+					<NotificationsIcon />
+					<Image
+						src="/../images/avatar.png"
+						alt="avatar-image"
+						width={100}
+						height={100}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { Topbar } from './components/Topbar';
+
+import styles from './layouts/Home.module.css';
 export default function Home() {
+	// return (
+	// 	<main className="flex min-h-screen flex-col items-center justify-between p-24">
+	// 		<Link href="/login">Login</Link>
+	// 	</main>
+	// );
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Link href="/login">Login</Link>
-		</main>
+		<div className={styles.containter}>
+			<Topbar />
+		</div>
 	);
 }
