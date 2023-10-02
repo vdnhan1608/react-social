45a5ec335c6fce4dbe5e8edcc01af648b482f25d
bunrealcua <voduynhan.123@gmail.com>
2023-10-02 15:23:58 +0700

    feature create post

--- src/app/components/Feed.tsx
+++ src/app/components/Feed.tsx
@@ -1,10 +1,12 @@
 import styles from '../layouts/Feed.module.css';
+import { Post } from './Post';
 import { StatusBox } from './StatusBox';
 
 export function Feed() {
 	return (
 		<div className={styles.container}>
 			<StatusBox />
+			<Post />
 		</div>
 	);
 }
---
+++ src/app/components/Post.tsx
@@ -0,0 +1,5 @@
+import styles from '../layouts/Post.module.css';
+
+export function Post() {
+	return <div className={styles.container}>Post</div>;
+}
